67aa8206a86a3c0e19613c549394ac15fc8867fe
shab2112 <shaibi2182@gmail.com>
2025-11-13 17:42:47 +0400

    Updated secrets.ts

--- lib/secrets.ts
+++ lib/secrets.ts
@@ -2,6 +2,6 @@ export const API_KEY = "ff7941e7-bf38-11f0-a99e-066a7fa2e369";
 export const API_URL = "https://api.liveavatar.com";
 export const AVATAR_ID = "0aae6046-0ab9-44fe-a08d-c5ac3f406d34";
 
-export const VOICE_ID = "ac277b338cf64d8b9686784c43c563da";
+export const VOICE_ID = "b2bd6569-a537-4342-aeca-a1f15d2a2c97";
 export const CONTEXT_ID = "5237a256-1c21-4b84-843e-6ac4ab8deb23";
 export const LANGUAGE = "en";
