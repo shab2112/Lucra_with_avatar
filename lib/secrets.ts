718d09e076aabb5c5a5b2adb42d037e31c1ccdd1
shab2112 <shaibi2182@gmail.com>
2025-11-13 18:12:12 +0400

    Updated secrets.ts

--- lib/secrets.ts
+++ lib/secrets.ts
@@ -1,7 +1,7 @@
-export const API_KEY = "ff7941e7-bf38-11f0-a99e-066a7fa2e369";
-export const API_URL = "https://api.liveavatar.com";
-export const AVATAR_ID = "075abc67-2fae-4548-8ca9-b815fcbd34c7";
+export const API_KEY = import.meta.env.VITE_LIVEAVATAR_API_KEY;
+export const API_URL = import.meta.env.VITE_LIVEAVATAR_API_URL;
+export const AVATAR_ID = import.meta.env.VITE_LIVEAVATAR_AVATAR_ID;
 
-export const VOICE_ID = "b2bd6569-a537-4342-aeca-a1f15d2a2c97";
-export const CONTEXT_ID = "5237a256-1c21-4b84-843e-6ac4ab8deb23";
-export const LANGUAGE = "en";
+export const VOICE_ID = import.meta.env.VITE_LIVEAVATAR_VOICE_ID;
+export const CONTEXT_ID = import.meta.env.VITE_LIVEAVATAR_CONTEXT_ID;
+export const LANGUAGE = import.meta.env.VITE_LIVEAVATAR_LANGUAGE;
